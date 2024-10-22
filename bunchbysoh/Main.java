b955bb8853f39421b05db6b56a4e701a94f8e812
Dhruthi28 <137291294+Dhruthi28@users.noreply.github.com>
2024-10-22 18:04:24 +0530

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
       if(soh>83){
         counts.healthy++;
-      } else if(soh>=63 && soh<=83){
+      } else if(soh>=63){
         counts.exchange++;
       } else{
         counts.failed++;
@@ -26,6 +26,7 @@ public class Main {
 
   static void testBucketingByHealth() {
     System.out.println("Counting batteries by SoH...\n");
+    
     //Original Test
     int[] presentCapacities1 = {113, 116, 80, 95, 92, 70};
     CountsBySoH counts1 = countBatteriesByHealth(presentCapacities1);
@@ -35,20 +36,20 @@ public class Main {
     System.out.println("Test 1 passed");
 
     //New test: boundary condition for healthy (83%)
-    int[] presentCapacities2 = {100,83*120/100}; //Should classify both as healthy
+    int[] presentCapacities2 = {(int)Math.round(120*0.83),120}; //Should classify both as healthy
     CountsBySoH counts2 = countBatteriesByHealth(presentCapacities2);
     assert(counts2.healthy == 2) : "Boundary test for healthy failed";
     System.out.println("Test 2 passed");
 
     //New test: boundary condition for exchange(63%)
-    int[] presentCapacities3 = {63*120/100, 83*120/100 -1}; //83% and 63% exactly
+    int[] presentCapacities3 = {(int)Math.round(120*0.63),(int)Math.round(120*0.83-1)}; //83% and 63% exactly
     CountsBySoH counts3 = countBatteriesByHealth(presentCapacities3);
     assert(counts3.healthy == 0): "Boundary test healthy(none) failed";
     assert(counts3.healthy == 2): "Boundary test exchange failed";
     System.out.println("Test 3 passed");
 
     //New test: failed battery at 0% capacity
-    int[] presentCapacities4 = {0,62*120/100}; //should classify both as failed
+    int[] presentCapacities4 = {0,(int)Math.round(120*0.62)}; //should classify both as failed
     CountsBySoH counts4 = countBatteriesByHealth(presentCapacities4);
     assert(counts4.failed == 2): "Failed boundary test failed";
     System.out.println("Test 4 passed");
